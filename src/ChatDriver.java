d26d6a5c57f79d04940c1079907bd2d75b499c53
Camber Boles <claragboles51@gmail.com>
2020-11-23 13:49:39 -0500

    Implemented panel with simple JLabel functionality

---
+++ src/ChatDriver.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * ChatDriver
+ * A GUI panel that a chat user interacts with.
+ *
+ * @author Camber Boles
+ * @version 20 November 2020
+ */
+public class ChatDriver extends JComponent {
+    public ChatDriver() {
+
+    }
+}
